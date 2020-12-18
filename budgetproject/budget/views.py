d703c16fad2dbc678be969d6f206a60cded899cc
Daniil Lebedev <daniilwork247@gmail.com>
2020-12-18 12:31:05 +0000

    Added database and app works more or less ok

--- budgetproject/budget/views.py
+++ budgetproject/budget/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 # Create your views here.
 def project_list(request):
 	return render(request,'budget/project-list.html')
 
 def project_detail(request, project_slug):
-	#fetching the correct project
-	return render(request,'budget/project-detail.html')
-
+	project = get_object_or_404(Project, slug=project_slug)
+	expanse_list = project.expanses
+	return render(request,'budget/project-detail.html',{'project':project, 'expanse_list': project.expanses.all()})
