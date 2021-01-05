615e7432027cf36b5ca82398d7c35eed7e72314e
Daniil Lebedev <daniilwork247@gmail.com>
2021-01-05 13:52:56 +0000

    Changed the requirements file and added new superuser to the database

--- budgetproject/budget/views.py
+++ budgetproject/budget/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project
 
-# Create your views here.
 def project_list(request):
-	return render(request,'budget/project-list.html')
+	return render(request,'budget/project-list.html')#returns budget webpage
 
 def project_detail(request, project_slug):
 	project = get_object_or_404(Project, slug=project_slug)
 	expanse_list = project.expanses
-	return render(request,'budget/project-detail.html',{'project':project, 'expanse_list': project.expanses.all()})
+	return render(request,'budget/project-detail.html',{'project':project, 'expanse_list': project.expanses.all()})#handels project requests
